2f9663ffcc26671503c3a2d450889e0f7caf2223
Connor <cguy33333@gmail.com>
2024-04-23 11:43:01 -0700

    Added all questions needed and code for writing files, as well as adding a new file to the library for rendering (may be replaced later).

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
+const fs = require(`fs`);
 const jest = require(`jest`);
 const inquirer = require(`inquirer`);
+const render = require(`./lib/render`);
+
+
+const questions = [
+    {
+        type: `input`,
+        name: `logo`,
+        message: `Enter up to 3 characters:`,
+        maxLength: 3
+    },
+    {
+        type: `input`,
+        name: `text-color`,
+        message: `What color should the text be? (color keyword or hexadecimal)`
+    },
+    {
+        type: `list`,
+        name: `shape`,
+        message: `Select a shape`,
+        choices: [`circle`, `triangle`, `square`]
+    },
+    {
+        type: `input`,
+        name: `shape-color`,
+        message: `What color should the shape be? (color keyword or hexadecimal)`
+    }
+]
+
+function createSVG (fileName, data) {
+    // I want each 3 letter combination to have unique filenames
+    return fs.writeFileSync(`./examples/${fileName}`, data);
+}
+
+function init () {
+    inquirer.prompt(questions).then((responses) => {
+        console.log(`Generated logo.svg`);
+        createSVG(`${responses.logo}.svg`, render(responses))
+    })
+}
+
+init();
